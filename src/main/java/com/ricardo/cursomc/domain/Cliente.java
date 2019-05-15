c5075e2c8184b9a72570c113d437ab93805640f0
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-05-15 10:36:03 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/ricardo/cursomc/domain/Cliente.java
+++ src/main/java/com/ricardo/cursomc/domain/Cliente.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.ricardo.cursomc.domain.enums.TipoCliente;
 
@@ -30,6 +31,7 @@ public class Cliente implements Serializable {
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	
 	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
@@ -39,6 +41,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	@JsonBackReference
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
---
+++ src/main/java/com/ricardo/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.ricardo.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ricardo.cursomc.domain.Pedido;
+import com.ricardo.cursomc.repositories.PedidoRepository;
+import com.ricardo.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Pedido.class.getName());
+		}
+		
+		return obj;
+	}
+}
