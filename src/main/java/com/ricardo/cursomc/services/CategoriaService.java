f3d85d06b463d0b820bf9c8191c3685cacfdae55
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-05-17 11:29:04 -0300

    Correcoes: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/ricardo/cursomc/services/CategoriaService.java
+++ src/main/java/com/ricardo/cursomc/services/CategoriaService.java
@@ -10,63 +10,70 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ricardo.cursomc.domain.Categoria;
+import com.ricardo.cursomc.domain.Cliente;
 import com.ricardo.cursomc.dto.CategoriaDTO;
 import com.ricardo.cursomc.repositories.CategoriaRepository;
 import com.ricardo.cursomc.services.exceptions.DataIntegrityException;
 import com.ricardo.cursomc.services.exceptions.ObjectNotFoundException;
-
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Categoria obj = repo.findOne(id);
 		if (obj == null) {
-			throw new ObjectNotFoundException("Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName());
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName());
 		}
-		
+
 		return obj;
 	}
-	
-	//metodo para inserir a gategoria usando o repo.
+
+	// metodo para inserir a gategoria usando o repo.
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
-	//implementado metodo update
+
+	// implementado metodo update
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
-		
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+
 	}
-	
-	//metodo para realizar o delete
+
+	// metodo para realizar o delete
 	public void delete(Integer id) {
 		find(id);
 		try {
-		repo.delete(id);
-		}catch(DataIntegrityViolationException e) {
-			
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
+
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos!");
 		}
 	}
-	
-	//metodo para listar categorias
-	public List<Categoria> findAll(){
+
+	// metodo para listar categorias
+	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-	
-	//Função para controlar a paginação
-	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
-		PageRequest pageRequest = new PageRequest(page, linesPerPage,Direction.valueOf(direction), orderBy);
+
+	// Função para controlar a paginação
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Categoria fromDTO(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
+
 	}
 }
