960ba42f0b4cfe76e057440b2de0fefa26803d88
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-05-20 10:03:19 -0300

    Personalisando o retorno da validação

---
+++ src/main/java/com/ricardo/cursomc/resources/exception/FieldMessage.java
@@ -0,0 +1,38 @@
+package com.ricardo.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private String fieldname;
+	private String message;
+	
+	public FieldMessage() {
+	}
+
+	public FieldMessage(String name, String msg) {
+		super();
+		this.fieldname = name;
+		this.message = msg;
+	}
+
+	public String getFieldname() {
+		return fieldname;
+	}
+
+	public void setFieldname(String fieldname) {
+		this.fieldname = fieldname;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	
+
+}
--- src/main/java/com/ricardo/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/ricardo/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -12,19 +14,31 @@ import com.ricardo.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
+
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StantardError> objsctNotFound(ObjectNotFoundException e, HttpServletRequest request){
-		
+	public ResponseEntity<StantardError> objsctNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+
 		StantardError err = new StantardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
-	
+
 	@ExceptionHandler(DataIntegrityException.class)
-	public ResponseEntity<StantardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
-		
-		StantardError err = new StantardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+	public ResponseEntity<StantardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+
+		StantardError err = new StantardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(),
+				System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
-	
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StantardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação",
+				System.currentTimeMillis());
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+
 }
