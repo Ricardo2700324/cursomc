b5452d5f3282541993e20ef4e36647ae7947cb62
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-05-30 10:04:42 -0300

    Implementando autorizacao token

--- src/main/java/com/ricardo/cursomc/config/SecurityConfig.java
+++ src/main/java/com/ricardo/cursomc/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.ricardo.cursomc.security.JWTAuthenticationFilter;
+import com.ricardo.cursomc.security.JWTAuthorizationFilter;
 import com.ricardo.cursomc.security.JWTUtil;
 
 @Configuration
@@ -61,6 +62,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
