3c85e338ca52a70dacac041b3b4f14b5c12af7bb
Ricardo Fernando de Souza <rfs.br@hotmail.com>
2019-06-06 11:44:33 -0300

    Bonus: ajustando tamanho da imagem

--- src/main/java/com/ricardo/cursomc/services/ClienteService.java
+++ src/main/java/com/ricardo/cursomc/services/ClienteService.java
@@ -53,6 +53,9 @@ public class ClienteService {
 
 	@Value("${img.prefix.client.profile}")
 	private String prefix;
+	
+	@Value("${img.profile.size}")
+	private Integer size;
 
 	public Cliente find(Integer id) {
 
@@ -144,6 +147,10 @@ public class ClienteService {
 		}
 
 		BufferedImage jpgImage = imageService.getJpgImageFromFile(multpartFile);
+		jpgImage = imageService.cropSquare(jpgImage);
+		jpgImage = imageService.resize(jpgImage, size);
+		
+		
 		String fileName = prefix + user.getId() + ".jpg";
 
 		return s3Service.uploadFile(imageService.getInputStream(jpgImage, "jpg"), fileName, "image");
--- src/main/java/com/ricardo/cursomc/services/ImageService.java
+++ src/main/java/com/ricardo/cursomc/services/ImageService.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import javax.imageio.ImageIO;
 
 import org.apache.commons.io.FilenameUtils;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,23 +18,23 @@ import com.ricardo.cursomc.services.exceptions.FileException;
 
 @Service
 public class ImageService {
-	
+
 	public BufferedImage getJpgImageFromFile(MultipartFile uploadedFile) {
 		String ext = FilenameUtils.getExtension(uploadedFile.getOriginalFilename());
-		if(!"png".equals(ext) && !"jpg".equals(ext)) {
+		if (!"png".equals(ext) && !"jpg".equals(ext)) {
 			throw new FileException("Somente imagens JPG e PNG são permitidas!");
 		}
-		
+
 		try {
 			BufferedImage img = ImageIO.read(uploadedFile.getInputStream());
-			if("png".equals(ext)) {
+			if ("png".equals(ext)) {
 				img = pngToJpg(img);
 			}
 			return img;
 		} catch (IOException e) {
 			throw new FileException("Erro ao ler arquivo!");
 		}
-		
+
 	}
 
 	public BufferedImage pngToJpg(BufferedImage img) {
@@ -51,5 +52,21 @@ public class ImageService {
 			throw new FileException("Erro ao ler arquivo");
 		}
 	}
+
+	public BufferedImage cropSquare(BufferedImage sourceImg) {
+		int min = (sourceImg.getHeight() <= sourceImg.getWidth()) ? sourceImg.getHeight() : sourceImg.getWidth();
+		return Scalr.crop(
+				sourceImg, 
+				(sourceImg.getWidth() / 2) - (min / 2),
+				(sourceImg.getHeight() / 2) - (min / 2),
+				min,
+				min);
+
+	}
+	
+	public BufferedImage resize(BufferedImage sourceImg, int size) {
+		return Scalr.resize(sourceImg, Scalr.Method.ULTRA_QUALITY, size);
+	}
+	
 
 }
